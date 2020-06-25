3ed34d6381e05c43a31a4d5df97c511c462ed937
noobalpha <31156685+shelcia@users.noreply.github.com>
2020-06-25 23:52:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let temp = "";
 let empty_pos = 16;
+let shuffle_dificulty = 1;
 const move = id => {
   let movable_pos = exchangeable(id);
-  console.log(movable_pos);
-  console.log("empty " + empty_pos);
+  // console.log(movable_pos);
+  // console.log("empty " + empty_pos);
   let constrictMoment = isThere(movable_pos, empty_pos);
   if (constrictMoment) {
     let tileprev = document.getElementById(id);
@@ -16,7 +17,7 @@ const move = id => {
     empty_pos = id;
     // console.log(" new empty " + empty_pos);
   } else {
-    console.log("you cant change");
+    console.log("you can't change");
   }
 };
 //in order to constraint motion
@@ -47,12 +48,34 @@ const isThere = (movable_pos, empty_pos) => {
   if (k == 0) return false;
   else return true;
 };
+const select_difficulty = () => {
+  let difficulty_option = document.getElementById("Difficulty").selectedIndex;
+  shuffle_dificulty = document.getElementsByTagName("option")[difficulty_option]
+    .value;
+};
 const shuffle = () => {
   let rand;
-  for (let i = 0; i < 300; i++) {
-    rand = Rand();
-    console.log(rand);
-    move(rand);
+  select_difficulty();
+  console.log(shuffle_dificulty);
+  switch (shuffle_dificulty) {
+    case "1":
+      for (let i = 0; i < 100; i++) {
+        rand = Rand();
+        move(rand);
+      }
+      break;
+    case "2":
+      for (let i = 0; i < 200; i++) {
+        rand = Rand();
+        move(rand);
+      }
+      break;
+    case "3":
+      for (let i = 0; i < 300; i++) {
+        rand = Rand();
+        move(rand);
+      }
+      break;
   }
 };
 
@@ -61,4 +84,131 @@ const Rand = () => {
   let max = 16;
   let random = Math.floor(Math.random() * (+max - +min)) + +min;
   return random;
+};
+
+const correctPos = () => {
+  let a1 = document.getElementById("1");
+  // console.log(a1.innerHTML);
+  let a2 = document.getElementById("2");
+  let a3 = document.getElementById("3");
+  let a4 = document.getElementById("4");
+  let a5 = document.getElementById("5");
+  let a6 = document.getElementById("6");
+  let a7 = document.getElementById("7");
+  let a8 = document.getElementById("8");
+  let a9 = document.getElementById("9");
+  let a10 = document.getElementById("10");
+  let a11 = document.getElementById("11");
+  let a12 = document.getElementById("12");
+  let a13 = document.getElementById("13");
+  let a14 = document.getElementById("14");
+  let a15 = document.getElementById("15");
+  if (
+    a1.innerHTML == "1" &&
+    a2.innerHTML == "2" &&
+    a3.innerHTML == "3" &&
+    a4.innerHTML == "4" &&
+    a5.innerHTML == "5" &&
+    a6.innerHTML == "6" &&
+    a7.innerHTML == "7" &&
+    a8.innerHTML == "8" &&
+    a9.innerHTML == "9" &&
+    a10.innerHTML == "10" &&
+    a11.innerHTML == "11" &&
+    a12.innerHTML == "12" &&
+    a13.innerHTML == "13" &&
+    a14.innerHTML == "14" &&
+    a15.innerHTML == "15"
+  ) {
+    return "Congratulations .. You are indeed a master peice";
+  } else {
+    return "Better luck next time.. We feel you bruh!";
+  }
+};
+
+// const coins =()=>{
+//   let result=correctPos()
+//   if(result=="Congratulations .. You are indeed a master peice"){
+//     get_time
+//   }
+// }
+
+//timer global variables
+let startTime;
+let updatedTime;
+let difference;
+let tInterval;
+let savedTime;
+let paused = 0;
+let running = 0;
+let timerDisplay = document.getElementById("timer");
+
+const startTimer = () => {
+  if (!running) {
+    startTime = new Date().getTime();
+    tInterval = setInterval(getShowTime, 1);
+    paused = 0;
+    running = 1;
+  }
+  shuffle();
+};
+const resetTimer = () => {
+  clearInterval(tInterval);
+  savedTime = 0;
+  difference = 0;
+  paused = 0;
+  running = 0;
+  seconds = "0";
+  timerDisplay.innerHTML = seconds + "s";
+};
+const getShowTime = () => {
+  updatedTime = new Date().getTime();
+  if (savedTime) {
+    difference = updatedTime - startTime + savedTime;
+  } else {
+    difference = updatedTime - startTime;
+  }
+  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  timerDisplay.innerHTML = seconds + "s";
+  if (seconds == 59) {
+    // pauseTimer();
+    resetTimer();
+    correctPos();
+    resultModal();
+  }
+};
+let button_Modal = document.getElementById("Instructions");
+let modal = document.getElementById("myModal");
+let close_btn = document.getElementsByClassName("close")[0];
+button_Modal.onclick = function() {
+  modal.style.display = "block";
+};
+close_btn.onclick = function() {
+  modal.style.display = "none";
+};
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// let resbutton_Modal = document.getElementById("Instructions");
+let res_modal = document.getElementById("ResModal");
+let res_modal_content = document.getElementById("modal-content");
+let res_close_btn = document.getElementById("resclose");
+
+const resultModal = () => {
+  res_modal.style.display = "block";
+  let result = correctPos();
+  console.log(result);
+  res_modal_content.innerHTML = result;
+};
+res_close_btn.onclick = function() {
+  res_modal.style.display = "none";
+};
+window.onclick = function(event) {
+  if (event.target == res_modal) {
+    res_modal.style.display = "none";
+  }
 };
